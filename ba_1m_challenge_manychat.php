f58bed5f9106db4bc9f6d751ef8311c43ae45f3d
Klaus Schwarzkopf <klaus1977@gmx.de>
2019-02-06 21:11:24 +0100

    Update ba_1m_challenge_manychat.php

--- ba_1m_challenge_manychat.php
+++ ba_1m_challenge_manychat.php
@@ -1,4 +1,5 @@
 <?php
+//https://use-shortcuts.de/webhooks/ba_1m_challenge_manychat.php?your_ba_id=493152&leader_name=Full Name
 $link = "https://challenge.mybuilderall.eu/#aid=".$_GET['your_ba_id']."&name=" . base64_encode($_GET['leader_name']);
 echo '{ "leader_link": "'.$link.'"  }';
 ?>
